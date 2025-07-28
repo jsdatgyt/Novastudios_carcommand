de948621f9be4f50d319f0a0e02629a8a727061c
Novastudio's <Joshua-Rotherham@outlook.com>
2025-07-28 03:49:15 +0100

    Create fxmainifest.lua

---
+++ fxmainifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'Novastudios'
+description 'Simple /car Command'
+version '1.0.0'
+
+client_scripts { 'client.lua' }
+server_scripts { 'server.lua' }
